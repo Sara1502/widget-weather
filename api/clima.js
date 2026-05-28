d14bfcc2b2f7464f3980faa7c28ab361290a7bfa
Sara1502 <sara.moreira1520@gmail.com>
2026-05-28 16:16:12 -0300

    Update clima.js

--- api/clima.js
+++ api/clima.js
@@ -1,13 +1,13 @@
-GPUShaderModule.exports = async function handler(req, res) {
-    const { latitude, longitude } = req.query;
+module.exports = async function handler(req, res) {
+    const { lat, lon } = req.query;
 
-    if (!latitude || !longitude) {
+    if (!lat || !lon) {
         return res.status(400).json({ error: 'Localização não encontrada' });
     }
 
     try {
         const response = await fetch(
-            `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${promess.env.OPENWEATHER_API_KEY}&lang=pt_br&units=metric`
+            `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.OPENWEATHER_API_KEY}&lang=pt_br&units=metric`
         );
         const data = await response.json();
         res.status(200).json(data);
